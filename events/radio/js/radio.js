9b6b5213cff221ef6b83267e03788162bf7aa7d5
Tan Theng Lee <thengleetan@gmail.com>
2020-05-20 15:38:46 +0800

    Add unsupported event listeners in react lifecycle event to create custom event listeners

---
+++ events/radio/js/radio.js
@@ -0,0 +1,78 @@
+// @format
+class Radio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleResize = this.handleResize.bind(this);
+    let order = props.order;
+    let i = 1;
+    this.state = {
+      outerStyle: this.getStyle(4, i),
+      innerStyle: this.getStyle(1, i),
+      selectedStyle: this.getStyle(2, i),
+      taggerStyle: {
+        top: order * 20,
+        width: 25,
+        height: 25
+      }
+    };
+  }
+
+  getStyle(i, m) {
+    let value = i * m;
+    return {
+      top: value,
+      bottom: value,
+      left: value,
+      right: value
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize(event) {
+    let w = 1 + Math.round(window.innerWidth / 300);
+    this.setState({
+      taggerStyle: {
+        top: this.props.order * w * 10,
+        width: w * 10,
+        height: w * 10
+      },
+      textStyle: {
+        left: w * 13,
+        fontSize: 7 * w
+      }
+    });
+  }
+
+  render() {
+    return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("div", {
+      className: "radio-tagger",
+      style: this.state.taggerStyle
+    }, /*#__PURE__*/React.createElement("input", {
+      type: "radio",
+      name: this.props.name,
+      id: this.props.id
+    }), /*#__PURE__*/React.createElement("label", {
+      htmlFor: this.props.id
+    }, /*#__PURE__*/React.createElement("div", {
+      className: "radio-text",
+      style: this.state.textStyle
+    }, this.props.label), /*#__PURE__*/React.createElement("div", {
+      className: "radio-outer",
+      style: this.state.outerStyle
+    }, /*#__PURE__*/React.createElement("div", {
+      className: "radio-inner",
+      style: this.state.innerStyle
+    }, /*#__PURE__*/React.createElement("div", {
+      className: "radio-selected",
+      style: this.state.selectedStyle
+    }))))));
+  }
+
+}
---
+++ events/radio/js/script.js
@@ -0,0 +1,17 @@
+// @format
+ReactDOM.render( /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(Radio, {
+  name: "radio-group",
+  id: "radio1",
+  order: "1",
+  label: "Credit Card"
+}), /*#__PURE__*/React.createElement(Radio, {
+  name: "radio-group",
+  id: "radio2",
+  order: "2",
+  label: "PayPal"
+}), /*#__PURE__*/React.createElement(Radio, {
+  name: "radio-group",
+  id: "radio3",
+  order: "3",
+  label: "Check"
+})), document.getElementById('content'));
