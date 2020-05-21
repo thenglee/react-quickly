374fa9fc78a889277a3d90aee90c024896ea91b5
Tan Theng Lee <thengleetan@gmail.com>
2020-05-21 14:23:07 +0800

    Form input checkbox elements

--- ch07-forms/elements/js/content.js
+++ ch07-forms/elements/js/content.js
@@ -3,14 +3,30 @@ class Content extends React.Component {
   constructor(props) {
     super(props);
     this.handleRadio = this.handleRadio.bind(this);
+    this.handleCheckbox = this.handleCheckbox.bind(this);
     this.state = {
-      selectedRadio: 'react'
+      selectedRadio: 'react',
+      checkboxGroup: {
+        node: false,
+        react: true,
+        express: false,
+        mongodb: false
+      }
     };
   }
 
   handleRadio(event) {
     this.setState({
       selectedRadio: event.target.value
+    });
+  }
+
+  handleCheckbox(event) {
+    let obj = this.state.checkboxGroup;
+    obj[event.target.value] = event.target.checked; // true or false
+
+    this.setState({
+      checkboxGroup: obj
     });
   }
 
@@ -33,7 +49,31 @@ class Content extends React.Component {
       value: "polymer",
       checked: this.state.selectedRadio === 'polymer',
       onChange: this.handleRadio
-    }), "Polymer"));
+    }), "Polymer"), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement("h2", null, "input: checkbox"), /*#__PURE__*/React.createElement("label", null, /*#__PURE__*/React.createElement("input", {
+      type: "checkbox",
+      name: "checkboxGroup",
+      value: "node",
+      checked: this.state.checkboxGroup['node'],
+      onChange: this.handleCheckbox
+    }), "Node"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("label", null, /*#__PURE__*/React.createElement("input", {
+      type: "checkbox",
+      name: "checkboxGroup",
+      value: "react",
+      checked: this.state.checkboxGroup['react'],
+      onChange: this.handleCheckbox
+    }), "React"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("label", null, /*#__PURE__*/React.createElement("input", {
+      type: "checkbox",
+      name: "checkboxGroup",
+      value: "express",
+      checked: this.state.checkboxGroup['express'],
+      onChange: this.handleCheckbox
+    }), "Express"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("label", null, /*#__PURE__*/React.createElement("input", {
+      type: "checkbox",
+      name: "checkboxGroup",
+      value: "mongodb",
+      checked: this.state.checkboxGroup['mongodb'],
+      onChange: this.handleCheckbox
+    }), "MongoDB"));
   }
 
 }
--- ch07-forms/elements/jsx/content.jsx
+++ ch07-forms/elements/jsx/content.jsx
@@ -4,14 +4,27 @@ class Content extends React.Component {
   constructor(props) {
     super(props);
     this.handleRadio = this.handleRadio.bind(this);
+    this.handleCheckbox = this.handleCheckbox.bind(this);
 
     this.state = {
       selectedRadio: 'react',
+      checkboxGroup: {
+        node: false,
+        react: true,
+        express: false,
+        mongodb: false,
+      },
     };
   }
 
   handleRadio(event) {
     this.setState({selectedRadio: event.target.value});
+  }
+
+  handleCheckbox(event) {
+    let obj = this.state.checkboxGroup;
+    obj[event.target.value] = event.target.checked; // true or false
+    this.setState({checkboxGroup: obj});
   }
 
   render() {
@@ -49,6 +62,51 @@ class Content extends React.Component {
             onChange={this.handleRadio}
           />
           Polymer
+        </label>
+        <hr />
+        <h2>input: checkbox</h2>
+        <label>
+          <input
+            type="checkbox"
+            name="checkboxGroup"
+            value="node"
+            checked={this.state.checkboxGroup['node']}
+            onChange={this.handleCheckbox}
+          />
+          Node
+        </label>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="checkboxGroup"
+            value="react"
+            checked={this.state.checkboxGroup['react']}
+            onChange={this.handleCheckbox}
+          />
+          React
+        </label>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="checkboxGroup"
+            value="express"
+            checked={this.state.checkboxGroup['express']}
+            onChange={this.handleCheckbox}
+          />
+          Express
+        </label>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="checkboxGroup"
+            value="mongodb"
+            checked={this.state.checkboxGroup['mongodb']}
+            onChange={this.handleCheckbox}
+          />
+          MongoDB
         </label>
       </form>
     );
