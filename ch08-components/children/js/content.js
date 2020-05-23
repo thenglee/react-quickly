d4aff619be159ab0920aae6ff06fd1ea0402d2b7
Tan Theng Lee <thengleetan@gmail.com>
2020-05-23 15:49:55 +0800

    Render children components

---
+++ ch08-components/children/js/content.js
@@ -0,0 +1,8 @@
+class Content extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement("div", {
+      className: "content"
+    }, this.props.children);
+  }
+
+}
---
+++ ch08-components/children/js/script.js
@@ -0,0 +1,7 @@
+ReactDOM.render( /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(Content, null, /*#__PURE__*/React.createElement("h1", null, "React"), /*#__PURE__*/React.createElement("p", null, "Rocks")), /*#__PURE__*/React.createElement(Content, null, /*#__PURE__*/React.createElement("img", {
+  src: "images/azat.jpg",
+  width: "100"
+})), /*#__PURE__*/React.createElement(Content, null, /*#__PURE__*/React.createElement("a", {
+  className: "btn btn-danger",
+  href: "http://react.rocks"
+}, "http://react.rocks"))), document.getElementById('content'));
