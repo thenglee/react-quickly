b159ac8acbda443270c4790428a9e4d04bfd6681
Tan Theng Lee <thengleetan@gmail.com>
2020-05-25 00:09:25 +0800

    Render more tooltips

--- ch10-tooltip-project/script.js
+++ ch10-tooltip-project/script.js
@@ -51,5 +51,15 @@ class Tooltip extends React.Component {
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(Tooltip, {
-  text: "The book you're reading now"
-}, "React Quickly"), " was published in 2017. It's awesome!"), document.getElementById('tooltip'));
+  text: "Master Express.js-The Node.js Framework For Your Web Development"
+}, "Pro Express.js"), ' ', "was published in 2014. It was one of the first books on v4.x. And it was my second book published with Apress after", ' ', /*#__PURE__*/React.createElement(Tooltip, {
+  text: "Practical Node.js: Building Real-World Scalable Web Apps"
+}, "Practical Node.js"), ". ... The main focus of this post is to compare the four Node.js/Io.js frameworks: ", /*#__PURE__*/React.createElement(Tooltip, {
+  text: "HTTP API server"
+}, "Hapi"), ",", ' ', /*#__PURE__*/React.createElement(Tooltip, {
+  text: "Release the Kraken!"
+}, "Kraken"), ",", ' ', /*#__PURE__*/React.createElement(Tooltip, {
+  text: "Sail away"
+}, "Sails.js"), " and", ' ', /*#__PURE__*/React.createElement(Tooltip, {
+  text: "IBM of frameworks"
+}, "Loopback"), ". There are many other frameworks to consider, but I had to draw the line somewhere."), document.getElementById('tooltip'));
