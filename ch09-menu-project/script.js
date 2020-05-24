6c04224bd1e6b03052ae443e57e5408d86804d7c
Tan Theng Lee <thengleetan@gmail.com>
2020-05-24 16:16:17 +0800

    Implemenet menu without jsx

---
+++ ch09-menu-project/script.js
@@ -0,0 +1,42 @@
+// @format
+
+class Link extends React.Component {
+  render() {
+    const url =
+      '/' +
+      this.props.label
+        .toLowerCase()
+        .trim()
+        .replace(' ', '-');
+
+    return React.createElement(
+      'div',
+      null,
+      React.createElement('a', {href: url}, this.props.label),
+      React.createElement('br'),
+    );
+  }
+}
+
+class Menu extends React.Component {
+  render() {
+    let menus = ['Home', 'About', 'Services', 'Portfolio', 'Contact us']; // mock data store
+
+    return React.createElement(
+      'div',
+      null,
+      menus.map((v, i) => {
+        return React.createElement(
+          'div',
+          {key: i},
+          React.createElement(Link, {label: v}),
+        );
+      }),
+    );
+  }
+}
+
+ReactDOM.render(
+  React.createElement(Menu, null),
+  document.getElementById('menu'),
+);
