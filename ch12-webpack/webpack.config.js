6fd825cac178cd6c226be9ad8240a28db27c2cbb
Tan Theng Lee <thengleetan@gmail.com>
2020-05-26 23:38:06 +0800

    Update email project to use webpack

---
+++ ch12-webpack/webpack.config.js
@@ -0,0 +1,16 @@
+// @format
+
+module.exports = {
+  entry: './jsx/app.jsx',
+  output: {
+    path: __dirname + '/js/',
+    filename: 'bundle.js',
+  },
+  devtool: '#sourcemap',
+  module: {
+    loaders: [
+      {test: /\.css$/, loader: 'style-loader!css-loader'},
+      {test: /\.jsx?$/, exclude: /(node_modules)/, loaders: ['babel-loader']},
+    ],
+  },
+};
