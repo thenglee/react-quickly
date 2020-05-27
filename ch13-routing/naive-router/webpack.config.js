9fba11053152fa6138264f72fe73e016efb6c9dd
Tan Theng Lee <thengleetan@gmail.com>
2020-05-28 00:15:03 +0800

    Implement routing

---
+++ ch13-routing/naive-router/webpack.config.js
@@ -0,0 +1,18 @@
+// @format
+
+module.exports = {
+  entry: './jsx/app.jsx',
+  output: {
+    path: __dirname + '/js/',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
